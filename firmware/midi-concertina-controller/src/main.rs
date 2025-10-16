7a8e40486990af8639bb5327667b6924b1cf94a6
Steven Schulteis <schult@gmail.com>
2025-10-16 18:05:52 -0500

    Perform LED blink test in controller firmware

--- firmware/midi-concertina-controller/src/main.rs
+++ firmware/midi-concertina-controller/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use embassy_stm32::gpio;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    let config = embassy_stm32::Config::default();
+    let dp = embassy_stm32::init(config);
+
+    let mut led = gpio::Output::new(dp.PB4, gpio::Level::High, gpio::Speed::Low);
+    loop {
+        cortex_m::asm::delay(3_000_000);
+        led.toggle();
+    }
 }
