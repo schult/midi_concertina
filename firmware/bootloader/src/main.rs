05446d04b0ec50e883486756f7f79c0912f85f94
Steven Schulteis <schult@gmail.com>
2025-11-11 15:35:38 -0600

    Simplify bootloader

--- firmware/bootloader/src/main.rs
+++ firmware/bootloader/src/main.rs
@@ -8,7 +8,6 @@ use embassy_boot_stm32::{BootLoader, BootLoaderConfig};
 use embassy_stm32::flash::{self, Flash};
 use embassy_stm32::rcc;
 use embassy_sync::blocking_mutex::Mutex;
-use embedded_storage::nor_flash::NorFlash;
 use panic_reset as _;
 
 #[entry]
@@ -18,12 +17,12 @@ fn main() -> ! {
     config.rcc.sys = rcc::Sysclk::HSI;
     let p = embassy_stm32::init(config);
 
-    let flash_layout = Flash::new_blocking(p.FLASH).into_blocking_regions();
-    let flash = Mutex::new(RefCell::new(flash_layout.bank1_region));
+    let flash = Flash::new_blocking(p.FLASH);
+    let flash = Mutex::new(RefCell::new(flash));
 
     let config = BootLoaderConfig::from_linkerfile_blocking(&flash, &flash, &flash);
     let active_offset = config.active.offset();
-    const PAGE_SIZE: usize = Flash::<flash::Blocking>::ERASE_SIZE;
+    const PAGE_SIZE: usize = flash::BANK1_REGION.erase_size as usize;
     let bl = BootLoader::prepare::<_, _, _, PAGE_SIZE>(config);
 
     unsafe { bl.load(flash::BANK1_REGION.base + active_offset) }
