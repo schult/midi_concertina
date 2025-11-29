6e8e009c7dd664ca0c9be0f5f0c9fb1f4097cf00
Steven Schulteis <schult@gmail.com>
2025-11-29 16:41:27 -0600

    Add enum to store any midi message type

--- firmware/midi/src/lib.rs
+++ firmware/midi/src/lib.rs
@@ -7,3 +7,20 @@ pub mod util;
 
 pub use channel_voice_message::{Channel, ChannelVoiceMessage, Note, cc};
 pub use system_exclusive_message::{ALL_CALL_DEVICE_ID, SystemExclusiveMessage};
+
+pub enum Message {
+    ChannelVoice(ChannelVoiceMessage),
+    SystemExclusive(SystemExclusiveMessage),
+}
+
+impl From<ChannelVoiceMessage> for Message {
+    fn from(value: ChannelVoiceMessage) -> Self {
+        Message::ChannelVoice(value)
+    }
+}
+
+impl From<SystemExclusiveMessage> for Message {
+    fn from(value: SystemExclusiveMessage) -> Self {
+        Message::SystemExclusive(value)
+    }
+}
