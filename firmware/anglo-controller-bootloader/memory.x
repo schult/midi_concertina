2b8c756710b65e5de210be15e495136e01078b5f
Steven Schulteis <schult@gmail.com>
2026-06-07 18:47:00 -0500

    Update controller firmware for MCU with 192kB flash

---
+++ firmware/anglo-controller-bootloader/memory.x
@@ -0,0 +1,18 @@
+MEMORY
+{
+    FLASH            : ORIGIN = 0x08000000, LENGTH = 8K
+    BOOTLOADER_STATE : ORIGIN = 0x08002000, LENGTH = 0x2C00
+    ACTIVE           : ORIGIN = 0x08004C00, LENGTH = 0x15980
+    DFU              : ORIGIN = 0x0801A600, LENGTH = 0x15A00
+
+    RAM              : ORIGIN = 0x20000000, LENGTH = 20K
+}
+
+__bootloader_state_start = ORIGIN(BOOTLOADER_STATE) - ORIGIN(FLASH);
+__bootloader_state_end = ORIGIN(BOOTLOADER_STATE) + LENGTH(BOOTLOADER_STATE) - ORIGIN(FLASH);
+
+__bootloader_active_start = ORIGIN(ACTIVE) - ORIGIN(FLASH);
+__bootloader_active_end = ORIGIN(ACTIVE) + LENGTH(ACTIVE) - ORIGIN(FLASH);
+
+__bootloader_dfu_start = ORIGIN(DFU) - ORIGIN(FLASH);
+__bootloader_dfu_end = ORIGIN(DFU) + LENGTH(DFU) - ORIGIN(FLASH);
---
+++ firmware/anglo-controller-bootloader/src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+
+use cortex_m_rt::entry;
+use embassy_boot_stm32::{BootLoader, BootLoaderConfig};
+use embassy_stm32::flash::{self, Flash};
+use embassy_stm32::rcc;
+use embassy_sync::blocking_mutex::Mutex;
+use panic_reset as _;
+
+#[entry]
+fn main() -> ! {
+    let mut config = embassy_stm32::Config::default();
+    config.rcc.hsi = true;
+    config.rcc.sys = rcc::Sysclk::HSI;
+    let p = embassy_stm32::init(config);
+
+    let flash = Flash::new_blocking(p.FLASH);
+    let flash = Mutex::new(RefCell::new(flash));
+
+    let config = BootLoaderConfig::from_linkerfile_blocking(&flash, &flash, &flash);
+    let boot_address = flash::BANK1_REGION.base + config.active.offset();
+    const PAGE_SIZE: usize = flash::MAX_ERASE_SIZE;
+    let bl = BootLoader::prepare::<_, _, _, PAGE_SIZE>(config);
+
+    unsafe { bl.load(boot_address) }
+}
